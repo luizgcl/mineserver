f639fee01281ba19ac7406b1a2fef7a0b37c1a4c
Luiz Gustavo <62274995+luizgcl@users.noreply.github.com>
2025-12-04 12:42:55 -0300

    first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+java -DPaper.IgnoreJavaVersion=true -Dfile.encoding=UTF-8 -Xmx2G -Xms2G -jar runtime.jar
+PAUSE
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+java -DPaper.IgnoreJavaVersion=true -Dfile.encoding=UTF-8 -Xmx2G -Xms2G -jar runtime.jar
+
+read -p "Pressione ENTER para sair..."
